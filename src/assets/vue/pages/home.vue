4a69e18f4da14d85b1100cf04e903276588fba43
Andrei Kamarou <mr.akomarov@gmail.com>
2018-10-27 23:44:56 +0300

    First commit

--- src/assets/vue/pages/home.vue
+++ src/assets/vue/pages/home.vue
@@ -1,50 +1,67 @@
 <template>
-    <f7-page>
-        <f7-navbar>
-            <f7-nav-left>
-                <f7-link class="panel-open" open-panel="left" icon="fa fa-bars"></f7-link>
-            </f7-nav-left>
-            <div class="title">Framework7-Vue</div>
-            <f7-nav-right>
-                <f7-link class="searchbar-enable" data-searchbar=".searchbar-components" icon="fa fa-search"></f7-link>
-            </f7-nav-right>
-            <f7-searchbar class="searchbar-components" search-container=".components-list" search-in="a" expandable></f7-searchbar>
-        </f7-navbar>
-        <f7-block-title>Introduction</f7-block-title>
-        <f7-block inner>
-            <p>This is template for Framework7 v2, Vue and Phonegap.</p>
-        </f7-block>
-        <f7-block-title class="searchbar-found">Links</f7-block-title>
-        <f7-list class="components-list searchbar-found">
-            <f7-list-item link="/about/" title="About" panel-close>
-                <f7-icon slot="media" icon="fa fa-info fa-fw"></f7-icon>
-            </f7-list-item>
-            <f7-list-item link="/form/" title="Form" panel-close>
-                <f7-icon slot="media" icon="fa fa-vcard-o fa-fw"></f7-icon>
-            </f7-list-item>
-            <f7-list-item link="/dynamic-route/blog/11/post/23/" title="Dynamic Route" panel-close>
-                <f7-icon slot="media" icon="fa fa-rss fa-fw"></f7-icon>
-            </f7-list-item>
-            <f7-list-item link="/chat/" title="Chat" panel-close>
-                <f7-icon slot="media" icon="fa fa-user fa-fw"></f7-icon>
-            </f7-list-item>
-            <f7-list-item link="/vuex/" title="Vuex" panel-close>
-                <f7-icon slot="media" icon="fa fa-battery fa-fw"></f7-icon>
-            </f7-list-item>
-        </f7-list>
-        <f7-list class="searchbar-not-found">
-            <f7-list-item title="Nothing found"></f7-list-item>
-        </f7-list>
-        <f7-block-title class="searchbar-hide-on-search">Themes</f7-block-title>
-        <f7-list class="searchbar-hide-on-search">
-            <f7-list-item title="iOS Theme" external link="./index.html?theme=ios"></f7-list-item>
-            <f7-list-item title="Material (MD) Theme" external link="./index.html?theme=md"></f7-list-item>
-            <f7-list-item title="Color Themes" link="/color-themes/"></f7-list-item>
-            <f7-list-item title="Github" external link="https://github.com/kevinqqnj"></f7-list-item>
-        </f7-list>
-    </f7-page>
+  <f7-page>
+    <f7-navbar class="navbar color-theme-red">
+      <f7-nav-left>
+        <f7-link class="panel-open" open-panel="left" icon="fa fa-bars"></f7-link>
+      </f7-nav-left>
+      <div class="title">Speakintelligence</div>
+      <f7-nav-right>
+        <f7-link class="searchbar-enable" data-searchbar=".searchbar-components" icon="fa fa-search"></f7-link>
+      </f7-nav-right>
+      <f7-searchbar class="searchbar-components" search-container=".components-list" search-in="a" expandable></f7-searchbar>
+    </f7-navbar>
+
+    <f7-list class="searchbar-not-found">
+      <f7-list-item title="Nothing found"></f7-list-item>
+    </f7-list>
+    <div class="hero-section">
+      <h1 class="hero-title">Your Fully Automated Communications Webshop</h1>
+      <p class="hero-subtitle">Simply add our customizable widget on your website and start increasing your revenue immediately</p>
+      <div class="row">
+        <div class="col-20"></div>
+        <div class="col-60">
+          <a class="button button-round button-fill button-raised color-red schedule-btn">Schedule Your Demo</a>
+        </div>
+        <div class="col-20"></div>
+      </div>
+      
+    </div>
+  </f7-page>
 </template>
 <script>
-export default {};
+  export default {};
 
 </script>
+
+
+<style>
+  .hero-section {
+    height: 100vh;
+    padding: 50px 0px;
+    background: #F8D353;
+    background: -webkit-linear-gradient(bottom right, #F8D353, #FF5C5A);
+    background: -moz-linear-gradient(bottom right, #F8D353, #FF5C5A);
+    background: linear-gradient(bottom right, #F8D353, #FF5C5A);
+  }
+
+  .hero-title {
+    text-align: center;
+    line-height: 1.2;
+    font-size: 28px;
+    margin-top: 6px;
+    color: #fff;
+  }
+
+  .hero-subtitle {
+    text-align: center;
+    line-height: 1.2;
+    font-size: 16px;
+    padding: 10px;
+    color: #fff;
+  }
+
+  .schedule-btn {
+    margin-top: 20px;
+  }
+
+</style>
---
+++ src/assets/vue/pages/splashscreen.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="splashscreen-wrapper">
+    <transition @leave="leaveEl" v-on:after-appear="leaveEl"  appear>
+      <div class="logo-wrapper">
+      </div>
+     </transition>
+    <div class="logo-letters-group">
+      <span class="logo-letter" v-for="(item, index) in logoName" :key="index" v-if="item !== 'img'">{{item}}</span>
+      <span class="logo-letter" v-else > <img src="https://geekyants.com/images/logo-icon-lg.png" /> </span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SplashScreen',
+  data () {
+    return {
+      show: true,
+      logoName: ['B', 'U', 'I', 'L', 'T', '  ', 'W', 'I', 'T', 'H', '  ', '<3', '  ', 'A', 'T', '  ', 'img', '  ', 'G', 'E', 'E', 'K', 'Y', 'A', 'N', 'T', 'S']
+    }
+  },
+  methods: {
+    leaveEl (el) {
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/splashScreen.scss'
+</style>
